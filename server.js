1e4be61c5220f540e026740c621c58b2444b68d8
Anant Mathur <anant.mathur007@gmail.com>
2019-12-29 08:44:51 +0530

    Bootcamp Schema

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const morgan = require("morgan");
+const colors = require("colors");
 const mongoose = require("mongoose"); 
 
 require('dotenv').config()
@@ -16,7 +17,7 @@ mongoose.connect(process.env.MongoURI, {useNewUrlParser: true, useUnifiedTopolog
     console.log(err);
 
     else
-    console.log('Database Connected');
+    console.log('Database Connected'.cyan.bold);
 });
 mongoose.set('useFindAndModify', false);
 
@@ -28,12 +29,12 @@ if(process.env.NODE_ENV === 'development')
     app.use(morgan('dev'));
 }
 
-// Routes
+// Use Routes
 app.use("/api/v1/bootcamps", bootcamps);
 
 
 // Listen to the PORt
 const PORT = process.env.PORT || 3000
 app.listen(PORT, () => {
-    console.log(`Server Running on port ${PORT}`)
+    console.log(`Server Running on port ${PORT}`.cyan.bold)
 })
